0136ebccc4e00ff70a5d3a4efed1bc2dce3a8ec9
Aryan <contactaryan@gmail.com>
2025-02-25 23:24:45 +0545

    fixed-project-3

--- project3.py
+++ project3.py
@@ -1,77 +1,83 @@
-# Tic Tac Toe Game
-def print_board(board): 
-    for i, row in enumerate(board): 
+#Tic Tac Toe Game (on Terminal)
+
+def print_board(board):
+    for i, row in enumerate(board):
         row_str = " "
-        for j, value in enumerate(row): 
-            row_str += value 
-            if j != len(row) -1: 
+        for j, value in enumerate(row):
+            row_str += value
+            if j != len(row) -1:
                 row_str += " | "
         print(row_str)
-        if i != len(board) -1: 
+        if i != len(board) -1 :
             print("-----------")
-def get_move(turn, board): 
-    while True: 
-        row = int(input("Row: "))
-        col = int(input("Col: "))
 
-        if row < 0 or row > len(board): 
-            print("Invalid Row, Try again")
-        elif col < 1 or col > len(board[row - 1]): 
-            print("Invalid Column, Try Again")
-        elif board[row - 1][col -1] != " ": 
-            print("Already Taken, Try Again!")
-        else: 
-            break
+def get_move(turn, board):
+    while True:
+        try:
+            row = int(input("Row: "))
+            col = int(input("Column: "))
 
-    board[row - 1][col - 1] = turn 
-
-def check_win(board, turn): 
-    lines = [
-        [(0, 0), (0, 1), (0, 2)],
-        [(1, 0), (1, 1), (1, 2)], 
-        [(2, 0), (2, 1), (2, 2)], 
-        [(0, 0), (1, 0), (2, 0)], 
-        [(0, 1), (1, 1), (2, 1)], 
-        [(0, 2), (1, 2), (2, 2)], 
-        [(0, 0), (1, 1), (2, 2)], 
-        [(0, 2), (1, 1), (2, 0)]
-    ]
-    for line in lines: 
-        win = True
-        for pos in line: 
-            row, col = pos
-            if board[row][col] != turn: 
-                win = False
-                break 
-        
-        if win: 
-            return True 
-    return False
-board = [ 
+            if row < 1 or row > len(board):
+                print("Invalid Row, Please Try again.")
+            elif col < 1 or col > len(board[row -1]):
+                print("Invalid Column, Please Try again.")
+            elif board[row-1][col-1] != " ":
+                print("Already Taken! Please try an empty spot")
+            else:
+                break
+        except(ValueError):
+            print("Value Error Occred! Please enter valid input.")
+    board[row-1][col-1] = turn
+board = [
     [" ", " ", " "],
     [" ", " ", " "],
     [" ", " ", " "],
 ]
 
+def check_answer(turn, board):
+    lines = [
+        [(0, 0), (0, 1), (0, 2)],
+        [(1, 0), (1, 1), (1, 2)],
+        [(2, 0), (2, 1), (2, 2)],
+        [(0, 0), (1, 0), (2, 0)],
+        [(0, 1), (1, 1), (2, 1)],
+        [(0, 2), (1, 2), (2, 2)],
+        [(0, 0), (1, 1), (2, 2)],
+        [(0, 2), (1, 1), (2, 0)]
+    ]
+
+    for line in lines:
+        win = True
+        for pos in line:
+            row, col = pos
+            if board[row][col] != turn:
+                win = False
+                break
+
+        if win:
+            return True
+    return False
 
 turn = "X"
 turn_number = 0
-print_board(board) 
+print_board(board)
 
-while turn_number < 9: 
-    print("It is the", turn, "players turn. Please select your move")
+while turn_number < 9:
+    print("It is the", turn, "player's turn to move.")
     get_move(turn, board)
-    print_board(board)  
-    winner = check_win(board, turn) 
+    print_board(board)
+    winner =check_answer(turn, board)
     if winner:
-        break 
-    
-    if turn == "X": 
-        turn = "O"
-    else: 
-        turn = "X"
+        break
     turn_number += 1
-if turn_number == 9: 
-    print("tied game")
-else: 
-    print("The winner was ", turn)
+
+    if turn == "X":
+        turn = "O"
+    else:
+        turn = "X"
+
+if turn_number == 9:
+    print("Tie Game")
+else:
+    print(f"{turn} is the winner!")
+
